86fe0278eec8c196ff63d9ba3e0972d36f652ea9
Mateusz Tylka <mattyl@st.amu.edu.pl>
2024-02-12 14:12:04 +0100

    validation corrections

---
+++ app/challenges/models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class ChallengeInputModel(BaseModel):
+    title: str
+    description: str | None = None
+    type: str
+    main_metric: str
+    deadline: str | None = None
+    award: str | None = None
