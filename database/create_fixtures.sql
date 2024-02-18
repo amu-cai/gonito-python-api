255f2c63c7b9b37fd05dabbb5ff73a2e9b21d123
Mateusz <mattyl@st.amu.edu.pl>
2024-02-18 20:57:18 +0100

    metrics with rmse test

--- database/create_fixtures.sql
+++ database/create_fixtures.sql
@@ -7,22 +7,22 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS challenges (
     id serial PRIMARY KEY,
-    title VARCHAR (50) UNIQUE NOT NULL,
-	type VARCHAR (50) NOT NULL,
-	description VARCHAR (50) NOT NULL,
-	main_metric VARCHAR (50) NOT NULL,
-	best_score VARCHAR (50) NOT NULL,
-    deadline VARCHAR (50) NOT NULL,
-    award VARCHAR (50) NOT NULL,
+    title VARCHAR (500) UNIQUE NOT NULL,
+	type VARCHAR (500) NOT NULL,
+	description VARCHAR (500) NOT NULL,
+	main_metric VARCHAR (500) NOT NULL,
+	best_score VARCHAR (500) NOT NULL,
+    deadline VARCHAR (500) NOT NULL,
+    award VARCHAR (500) NOT NULL,
     readme VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS submission (
     id serial PRIMARY KEY,
-    challenge VARCHAR (50) UNIQUE NOT NULL,
-	submitter VARCHAR (50) NOT NULL,
-	description VARCHAR (50) NOT NULL,
-	dev_result VARCHAR (50) NOT NULL,
-	test_result VARCHAR (50) NOT NULL,
+    challenge VARCHAR (500) REFERENCES challenges (title),
+	submitter VARCHAR (500) NOT NULL,
+	description VARCHAR (500) NOT NULL,
+	dev_result VARCHAR (500) NOT NULL,
+	test_result VARCHAR (500) NOT NULL,
     timestamp VARCHAR (50) NOT NULL
 );
