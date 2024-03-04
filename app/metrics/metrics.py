4c3bc0b7803e6de8a94e216330209564678c7f9b
flaizdnag <andrzej.m.gajda@gmail.com>
2024-03-04 08:37:57 +0100

    brier score

--- app/metrics/metrics.py
+++ app/metrics/metrics.py
@@ -4,15 +4,17 @@ from metric_base import MetricBase
 from accuracy import Accuracy
 from mse import MSE
 from rmse import RMSE
-from fbeta_score import FBeta
+from fbeta import FBeta
 from recall import Recall
 from precision import Precision
 from average_precision import AveragePrecision
 from balanced_accuracy import BalancedAccuracy
+from brier import Brier
 
 
 class Metrics(BaseModel):
     """All awailable metrics."""
+
     accuracy: MetricBase = Accuracy
     balanced_accuracy: MetricBase = BalancedAccuracy
     fbeta_score: MetricBase = FBeta
@@ -21,3 +23,4 @@ class Metrics(BaseModel):
     recall: MetricBase = Recall
     precision: MetricBase = Precision
     average_precision: MetricBase = AveragePrecision
+    brier: MetricBase = Brier
