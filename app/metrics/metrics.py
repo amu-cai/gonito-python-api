0055af5d5bffc7d968ee2c1868c45d3a6aa9af09
flaizdnag <andrzej.m.gajda@gmail.com>
2024-03-09 19:04:57 +0100

    general methods for metrics

--- app/metrics/metrics.py
+++ app/metrics/metrics.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Any
 
 from metric_base import MetricBase
 from accuracy import Accuracy
@@ -24,3 +25,51 @@ class Metrics(BaseModel):
     precision: MetricBase = Precision
     average_precision: MetricBase = AveragePrecision
     brier: MetricBase = Brier
+
+
+def all_metrics() -> list[str]:
+    """Show all available metrics."""
+    return Metrics.model_fields.keys()
+
+
+def metric_info(metric_name: str) -> dict:
+    """Get information about a metric."""
+    if metric_name not in all_metrics():
+        print(f"Metric {metric_name} is not defined")
+    else:
+        metric = getattr(Metrics(), metric_name)
+        return metric().info()
+
+
+def calculate_default_metric(
+    metric_name: str,
+    expected: list[Any],
+    out: list[Any],
+) -> Any:
+    """Use given metric with default settings."""
+    if metric_name not in all_metrics():
+        print(f"Metric {metric_name} is not defined")
+    else:
+        metric = getattr(Metrics(), metric_name)
+        return metric().calculate(expected, out)
+
+
+def calculate_metric(
+    metric_name: str,
+    expected: list[Any],
+    out: list[Any],
+) -> Any:
+    """Use given metric with non-default settings."""
+    if metric_name not in all_metrics():
+        print(f"Metric {metric_name} is not defined")
+    else:
+        metric = getattr(Metrics(), metric_name)
+        return metric().calculate(expected, out)
+
+
+print(f"accuracy fields: {Metrics().accuracy.model_fields}")
+print(f"accuracy info: {metric_info('accuracy')}")
+print(f"abc info: {metric_info('abc')}")
+print(f"accuracy calculate: {calculate_default_metric('accuracy', [1,2,3], [1,2,3])}")
+print(f"abc calculate: {calculate_default_metric('abc', [1,2,3], [1,2,3])}")
+# print(f"test: {test().info()}")
