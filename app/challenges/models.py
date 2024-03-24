0e9e69dd1887383fdec45d235ab1a5850ed55b09
flaizdnag <andrzej.m.gajda@gmail.com>
2024-03-24 23:09:28 +0100

    init validators

--- app/challenges/models.py
+++ app/challenges/models.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+
 
 class ChallengeInputModel(BaseModel):
     title: str
@@ -9,3 +10,29 @@ class ChallengeInputModel(BaseModel):
     deadline: str | None = None
     award: str | None = None
     challenge_source: str
+
+    @field_validator("title")
+    def title_validator(cls, value: str) -> str:
+        """Title field validation."""
+        if "dupa" in value:
+            raise ValueError("Use proper words for title")
+
+        return value
+
+
+example1 = ChallengeInputModel(
+    title="dupa blada",
+    main_metric_parameters="abc",
+    type="abc",
+    main_metric="abc",
+    challenge_source="abc"
+)
+example2 = ChallengeInputModel(
+    title="blada",
+    main_metric_parameters="abc",
+    type="abc",
+    main_metric="abc",
+    challenge_source="abc"
+)
+print(example1)
+print(example2)
